2e9230f263079d29f807da733440fc23fd7565cb
Yariv Nissim <yariv.nis@gmail.com>
2015-09-15 00:10:32 -0700

    Convert the project to Swift 2.0

--- MapKit/MapViewController.swift
+++ MapKit/MapViewController.swift
@@ -13,7 +13,6 @@ import CoreLocation
 class MapViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
-    
     private let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
@@ -23,24 +22,24 @@ class MapViewController: UIViewController {
     }
     
     @IBAction func addPin(gesture: UILongPressGestureRecognizer) {
-        if gesture.state == .Began {
-            let tapPoint = gesture.locationInView(mapView) // let vs. var
-            let coordinate = mapView.convertPoint(tapPoint, toCoordinateFromView: mapView)
+        guard gesture.state == .Began else { return }
+        
+        let tapPoint = gesture.locationInView(mapView) // let vs. var
+        let coordinate = mapView.convertPoint(tapPoint, toCoordinateFromView: mapView)
+        
+        // Note: Model object - represents data
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        annotation.title = "\(coordinate.latitude), \(coordinate.longitude)"
+        mapView.addAnnotation(annotation)
+        mapView.showAnnotations([annotation], animated: true)
+        
+        // Note: Optional
+        reverseGeocode(annotation) { placemark in
+            annotation.subtitle = annotation.title
+            annotation.title = placemark.name
             
-            // Note: Model object - represents data
-            let annotation = MKPointAnnotation()
-            annotation.coordinate = coordinate
-            annotation.title = "\(coordinate.latitude), \(coordinate.longitude)"
-            mapView.addAnnotation(annotation)
-            mapView.showAnnotations([annotation], animated: true)
-            
-            // Note: Optional
-            reverseGeocode(annotation) { placemark in
-                annotation.subtitle = annotation.title
-                annotation.title = placemark.name
-                
-                self.mapView.selectAnnotation(annotation, animated: true)
-            }
+            self.mapView.selectAnnotation(annotation, animated: true)
         }
     }
     
@@ -58,34 +57,54 @@ class MapViewController: UIViewController {
         presentViewController(searchController, animated: true, completion: nil)
     }
     
-    // Note: use enums for tags
-    private enum CalloutAction: Int {
-        case Delete = 1
-        case Navigate = 2
-    }
-    
     // Note: return an optional clousre async with default value
     private func reverseGeocode(annotation: MKAnnotation, completion: (CLPlacemark -> Void)? = nil) {
         let location = CLLocation(latitude: annotation.coordinate.latitude, longitude: annotation.coordinate.longitude)
         
         CLGeocoder().reverseGeocodeLocation(location) { (placemarks, _) in
-            if let placemark = placemarks?.first as? CLPlacemark {
-                completion?(placemark) // Note: Using optional closure
-            }
+            guard let placemark = placemarks?.first else { return }
+            completion?(placemark) // Note: Using optional closure
         }
     }
 }
 
+// Note: use enums for tags
+private enum CalloutAction: Int {
+    case Delete = 1
+    case Navigate = 2
+}
+
 // Note: Use extensions for protocols
+
+// MARK:- MKMapViewDelegate
+
 extension MapViewController: MKMapViewDelegate {
     // Note: Model vs. View in MVC
-    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
-        // Do not create a custom annotation view for User Location
-        if annotation.isKindOfClass(MKUserLocation.self) { return nil }
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        // Keep the default annotation view for User Location
+        guard !annotation.isKindOfClass(MKUserLocation.self) else { return nil }
         
         let reuseIdentifier = "PinView"
         let pinView: MKPinAnnotationView // Note: declare a constant without initialization
         
+        // Note: Use defer to prepare the annotation view before it's instantiated
+        defer {
+            // Prepare the annotation view (color, callout, etc.)
+            // for example: if annotation == ... then change color
+            pinView.pinColor = .Red
+        }
+        
+        // Note: Use a nested utility functions
+        func buttonWithTitle(title: String, type: CalloutAction) -> UIButton {
+            let button = UIButton(type: .System)
+            button.setTitle(title, forState: .Normal)
+            button.sizeToFit()
+            button.tag = type.rawValue
+            return button
+        }
+        
+        // Initialize the annotation view (dequeue or allocate)
+        //
         if let annotationView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseIdentifier) as? MKPinAnnotationView {
             // Dequeued an annotation view
             pinView = annotationView
@@ -97,58 +116,46 @@ extension MapViewController: MKMapViewDelegate {
             pinView.canShowCallout = true
             pinView.draggable = true
             
-            let deleteButton = UIButton.buttonWithType(.System) as! UIButton
-            deleteButton.setTitle("❌", forState: .Normal)
-            deleteButton.sizeToFit()
-            deleteButton.tag = CalloutAction.Delete.rawValue
-            pinView.rightCalloutAccessoryView = deleteButton
-            
-            let navigateButton = UIButton.buttonWithType(.System) as! UIButton
-            navigateButton.setTitle("🚘", forState: .Normal)
-            navigateButton.sizeToFit()
-            navigateButton.tag = CalloutAction.Navigate.rawValue
-            pinView.leftCalloutAccessoryView = navigateButton
+            pinView.rightCalloutAccessoryView = buttonWithTitle("❌", type: .Delete)
+            pinView.leftCalloutAccessoryView = buttonWithTitle("🚘", type: .Navigate)
         }
-        
-        // Prepare the annotation view (color, callout, etc.)
-        // if annotation == ... then change color
-        pinView.pinColor = .Red
-        
         return pinView
     }
     
-    func mapView(mapView: MKMapView!, annotationView view: MKAnnotationView!, calloutAccessoryControlTapped control: UIControl!) {
-        if let action = CalloutAction(rawValue: control.tag) {
-            switch action {
-            case .Delete:
-                mapView.removeAnnotation(view.annotation)
-                
-            case .Navigate:
-                if let annotation = view.annotation {
-                    reverseGeocode(annotation) { placemark in
-                        let item = MKMapItem(placemark: MKPlacemark(placemark: placemark))
-                        item.openInMapsWithLaunchOptions([MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving])
-                    }
-                }
+    func mapView(mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        guard let action = CalloutAction(rawValue: control.tag) else { return }
+        
+        switch action {
+        case .Delete:
+            guard let annotation = view.annotation else { return }
+            mapView.removeAnnotation(annotation)
+            
+        case .Navigate:
+            guard let annotation = view.annotation else { return }
+            
+            reverseGeocode(annotation) { placemark in
+                let item = MKMapItem(placemark: MKPlacemark(placemark: placemark))
+                item.openInMapsWithLaunchOptions([MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving])
             }
         }
     }
     
     // Note: Optional code, if we have time
-    func mapView(mapView: MKMapView!, annotationView view: MKAnnotationView!, didChangeDragState newState: MKAnnotationViewDragState, fromOldState oldState: MKAnnotationViewDragState) {
-        if newState == .Ending {
-            if let annotation = view.annotation as? MKPointAnnotation {
-                reverseGeocode(annotation) { placemark in
-                    annotation.subtitle = annotation.title
-                    annotation.title = placemark.name
-                }
-            }
+    func mapView(mapView: MKMapView, annotationView view: MKAnnotationView, didChangeDragState newState: MKAnnotationViewDragState, fromOldState oldState: MKAnnotationViewDragState) {
+        guard newState == .Ending, let annotation = view.annotation as? MKPointAnnotation
+            else { return }
+        
+        reverseGeocode(annotation) { placemark in
+            annotation.subtitle = annotation.title
+            annotation.title = placemark.name
         }
     }
 }
 
+// MARK:- CLLocationManagerDelegate
+
 extension MapViewController: CLLocationManagerDelegate {
-    func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
         // Note: match multiple values
         switch status {
         case .AuthorizedWhenInUse, .AuthorizedAlways:
@@ -164,28 +171,30 @@ extension MapViewController: CLLocationManagerDelegate {
     }
 }
 
+// MARK:- UISearchBarDelegate
+
 extension MapViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(searchBar: UISearchBar) {
-        let text = searchBar.text.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
-        
-        if !text.isEmpty {
-            let request = MKLocalSearchRequest()
-            request.naturalLanguageQuery = text
-            request.region = mapView.region
-            
-            let search = MKLocalSearch(request: request)
-            search.startWithCompletionHandler { (response, error) in
-                if response.mapItems.count > 0 {
-                    if let item = response.mapItems.first as? MKMapItem {
-                        let annotation = item.placemark
-                        self.mapView.addAnnotation(annotation)
-                        self.mapView.showAnnotations([annotation], animated: true)
-                        self.mapView.selectAnnotation(annotation, animated: true)
-                    }
-                }
-            }
+        // Note: use defer to dismiss the search controller even if the guard returns
+        defer {
+            dismissViewControllerAnimated(true, completion: nil)
         }
         
-        dismissViewControllerAnimated(true, completion: nil)
+        guard let text = searchBar.text?.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+            where !text.isEmpty else { return }
+        
+        let request = MKLocalSearchRequest()
+        request.naturalLanguageQuery = text
+        request.region = mapView.region
+        
+        let search = MKLocalSearch(request: request)
+        search.startWithCompletionHandler { (response, error) in
+            guard let item = response?.mapItems.first else { return }
+            
+            let annotation = item.placemark
+            self.mapView.addAnnotation(annotation)
+            self.mapView.showAnnotations([annotation], animated: true)
+            self.mapView.selectAnnotation(annotation, animated: true)
+        }
     }
 }
